044462c774e9fefba4c90801107b4e437454adfb
fiyintheslim <fiyinoyekunle@gmail.com>
2022-03-24 18:28:45 +0100

    fixes

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,11 @@ const server = app.listen(5050, ()=>{
 
 const io = new Server(server);
 
-<<<<<<< HEAD
+
 io.on("connection", (socket) => {
   socket.on("message", (data) => {
     socket.broadcast.emit("message", data);
   });
 });
-=======
-io.on("connection", (socket)=>{
-  console.log("user connected");
 
-  socket.on("message", (data) => {
-    console.log("message received", data);
-  });
-})
->>>>>>> 0fa616afb63dc4599ae6018ed6ca96a631691a8d
+
